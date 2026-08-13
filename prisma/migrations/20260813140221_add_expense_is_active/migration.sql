10a752c033a574f5df22bd28c2633d9a2a41b6ab
Salim Sambo <004sas@gmail.com>
2026-08-13 15:20:02 +0100

    Prepare for Railway deployment (migrations, CORS, start script)

---
+++ prisma/migrations/20260813140221_add_expense_is_active/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `expenses` ADD COLUMN `is_active` BOOLEAN NOT NULL DEFAULT true;
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   // Swagger setup
   const config = new DocumentBuilder()
