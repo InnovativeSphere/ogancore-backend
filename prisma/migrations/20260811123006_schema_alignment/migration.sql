3a5eb0f14260d1622079046e958adee2bb03d83e
Salim Sambo <004sas@gmail.com>
2026-08-13 18:59:53 +0100

    Fix migration table case for Railway

--- prisma/migrations/20260811123006_schema_alignment/migration.sql
+++ prisma/migrations/20260811123006_schema_alignment/migration.sql
@@ -1,24 +1,24 @@
 /*
   Warnings:
 
-  - You are about to drop the column `name` on the `branches` table. All the data in the column will be lost.
+  - You are about to drop the column `name` on the `BRANCHES` table. All the data in the column will be lost.
   - You are about to drop the column `file_path` on the `report_log` table. All the data in the column will be lost.
-  - You are about to drop the column `time` on the `report_schedule` table. All the data in the column will be lost.
+  - You are about to drop the column `time` on the `REPORT_SCHEDULE` table. All the data in the column will be lost.
   - Added the required column `branch_name` to the `BRANCHES` table without a default value. This is not possible if the table is not empty.
   - Added the required column `daily_time` to the `REPORT_SCHEDULE` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
-ALTER TABLE `branches` DROP COLUMN `name`,
+ALTER TABLE `BRANCHES` DROP COLUMN `name`,
     ADD COLUMN `branch_name` VARCHAR(100) NOT NULL,
     ADD COLUMN `is_active` BOOLEAN NOT NULL DEFAULT true;
 
 -- AlterTable
-ALTER TABLE `report_log` DROP COLUMN `file_path`,
+ALTER TABLE `REPORT_LOG` DROP COLUMN `file_path`,
     ADD COLUMN `file_location` VARCHAR(255) NULL;
 
 -- AlterTable
-ALTER TABLE `report_schedule` DROP COLUMN `time`,
+ALTER TABLE `REPORT_SCHEDULE` DROP COLUMN `time`,
     ADD COLUMN `daily_time` VARCHAR(5) NOT NULL;
 
 -- AlterTable
